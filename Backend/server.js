30c30300cd4fdd2ec254d11cec54f31afbc01cff
LiviuSilaghi <szilagyi.liviu@gmail.com>
2026-02-23 15:36:46 +0000

    1st

---
+++ Backend/server.js
@@ -0,0 +1,52 @@
+/** @format */
+
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+const morgan = require("morgan");
+const winston = require("winston");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+// Database connection
+
+mongoose
+  .connect(
+    process.env.MONGO_URI || "mongodb://localhost:27017/your-database-name",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+  )
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+// WinstonLogger setup
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json(),
+  ),
+  transports: [
+    new winston.transports.File({ filename: "error.log", level: "error" }),
+    new winston.transports.File({ filename: "combined.log" }),
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple(),
+      ),
+    }),
+  ],
+});
+
+app.use(
+  morgan(
+    ":method :url :status :response-time ms - :res ms - res[content-length]",
+  ),
+);
